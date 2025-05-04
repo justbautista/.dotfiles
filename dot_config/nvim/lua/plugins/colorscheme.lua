7e75eb226590a306c9265823d8a38def0f230706
justin <justinbautista923@gmail.com>
2025-05-04 18:59:23 -0400

    Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "material", -- Replace with your colorscheme
+      colorscheme = "catppuccin-mocha", -- Replace with your colorscheme
     },
   },
 
